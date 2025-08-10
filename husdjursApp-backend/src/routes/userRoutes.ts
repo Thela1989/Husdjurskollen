2923088746ad2337f4f8417a3d5a36fe663b055f
Therese Lagerhäll <tes_san_89@hotmail.com>
2025-08-10 22:05:30 +0200

    Update userRoutes.ts

--- husdjursApp-backend/src/routes/userRoutes.ts
+++ husdjursApp-backend/src/routes/userRoutes.ts
@@ -1,13 +1,28 @@
 import { Router, Request, Response } from "express";
 import pool from "../db";
+import jwt from "jsonwebtoken"
 
 const router = Router();
+//Middleware för att verifiera JWT-token
+function authenticateToken(req:any, res:Response, next: any){
+  const authHeader=req.header["authorization"];
+  const token= authHeader && authHeader.split(" ")[1];
+   if (!token) return res.sendStatus(401); //ingen token skickad
+
+   jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any)=>{
+    if(err) return res.sendStatus(403); //Ogiltig token
+    req.user = user; //user innehäller userId from jwt.sign()
+
+   })
+
+}
+
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const result = await pool.query(
-      "SELECT id, name, email FROM users WHERE id = $1",
+      "SELECT id, name, email FROM users WHERE id =
       [id]
     );
 
